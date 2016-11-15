447466c10cd58f0dab614dcdc41dd72fcfa27faf
David Anthoff <anthoff@berkeley.edu>
2016-11-14 22:03:20 -0800

    Progress

--- example/24-many-models.jl
+++ example/24-many-models.jl
@@ -2,6 +2,7 @@ using Query, DataFrames, GLM, RCall
 
 R"""
 library(gapminder)
+library(ggplot2)
 x <- gapminder
 """
 
@@ -10,9 +11,17 @@ df = @rget x
 q = @from i in df begin
     @group i by i.country into g
     @let model = lm(lifeExp ~ year, g)
-    @let residuals::Vector{Float64} = residuals(model)
-    @select residuals
-    @collect
+    @let residuals = residuals(model)
+    @select {country=g.key, data=zip(residuals, g)} into i
+    @from j in i.data
+    @select {country=convert(String, i.country), continent=convert(String,j[2].continent), j[2].year, j[2].lifeExp, resid=j[1]}
+    @collect DataFrame
 end
 
-println(q)
+# println(q)
+
+R"""
+ggplot($q, aes(year, resid, group = country)) +
+    geom_line(alpha = 1 / 3) + 
+    facet_wrap(~continent)
+"""
