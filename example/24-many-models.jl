3eac6760509a206024ed0fa3eaf9eb49ff249007
David Anthoff <anthoff@berkeley.edu>
2016-11-15 22:02:37 -0800

    Progress

--- example/24-many-models.jl
+++ example/24-many-models.jl
@@ -10,17 +10,20 @@ df = @rget x
 df[:country] = convert(Array{String,1},df[:country])
 df[:continent] = convert(Array{String,1},df[:continent])
 
+residuals2(mm::DataFrames.DataFrameRegressionModel) = residuals(mm.model)
 lm2(f,d) = convert(DataFrames.DataFrameRegressionModel{GLM.LinearModel{GLM.LmResp{Array{Float64,1}},GLM.DensePredQR{Float64}},Array{Float64,2}},lm(f, d))
 
 q = @from i in df begin
     @group i by i.country into g
     @let model = lm2(lifeExp ~ year, g)
-    @let residuals = residuals(model)
+    @let residuals = residuals2(model)
     @select {country=g.key, data=zip(residuals, g)} into i
     @from j in i.data
     @select {i.country, j[2].continent, j[2].year, j[2].lifeExp, resid=j[1]}
     @collect DataFrame
 end
+
+print(q)
 
 R"""
 ggplot($q, aes(year, resid, group = country)) +
