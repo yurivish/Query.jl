2e2202da9832814db8f57dba1ce46a3afed51a83
David Anthoff <anthoff@berkeley.edu>
2016-11-15 21:47:11 -0800

    Progress

--- example/24-many-models.jl
+++ example/24-many-models.jl
@@ -7,18 +7,20 @@ x <- gapminder
 """
 
 df = @rget x
+df[:country] = convert(Array{String,1},df[:country])
+df[:continent] = convert(Array{String,1},df[:continent])
+
+lm2(f,d) = convert(DataFrames.DataFrameRegressionModel{GLM.LinearModel{GLM.LmResp{Array{Float64,1}},GLM.DensePredQR{Float64}},Array{Float64,2}},lm(f, d))
 
 q = @from i in df begin
     @group i by i.country into g
-    @let model = lm(lifeExp ~ year, g)
+    @let model = lm2(lifeExp ~ year, g)
     @let residuals = residuals(model)
     @select {country=g.key, data=zip(residuals, g)} into i
     @from j in i.data
-    @select {country=convert(String, i.country), continent=convert(String,j[2].continent), j[2].year, j[2].lifeExp, resid=j[1]}
+    @select {i.country, j[2].continent, j[2].year, j[2].lifeExp, resid=j[1]}
     @collect DataFrame
 end
-
-# println(q)
 
 R"""
 ggplot($q, aes(year, resid, group = country)) +
